afe5fb1f82d7f52781e241b697d6e153e863b52f
unknown <hereiskeith@gmail.com>
2020-04-18 14:18:41 -0400

    Template Driven Forms and data binding

---
+++ tdf/src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { User } from "./user";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  topics = ['Angular', 'React', 'Vue'];
+  userModel = {
+    name: 'Bob',
+    email: 'Bob123@gmail.com',
+    phone: '',
+    topic: '',
+    timePreference: '',
+    subscribe: true
+  }
+}
